f016a221c45a92934697a676aaeae4976dbd321d
Ove Danner <ovedanner@srxp.com>
2015-10-21 17:04:16 +0200

    Fixed small bug when setting only one link

--- shape_traffic_client.py
+++ shape_traffic_client.py
@@ -11,6 +11,7 @@ Actually sends a throttle request to the given destination, telling
 it to limit bandwidth for a given IP to the given value.
 '''
 def send_throttle_request(destination, throttle_ip, bandwidth):
+	print "Going to send throttle request to " + destination
 	context = zmq.Context()
 	socket = context.socket(zmq.REQ)
 	socket.connect("tcp://" + destination + ":" + args.port)
@@ -32,7 +33,10 @@ if args.set:
 	if len(parts) < 3:
 		parser.print_usage()
 		sys.exit()
+
+	# Both ends need traffic shaping.
 	send_throttle_request(parts[0], parts[1], parts[2])
+	send_throttle_request(parts[1], parts[0], parts[2])
 
 # Limit one or multiple links in the given file.
 elif args.set_file:
