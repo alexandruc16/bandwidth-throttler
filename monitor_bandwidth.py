acb54da4a614bfff52a1c0f7a5f010f521346fa0
Ove Danner <ovedanner@gmail.com>
2015-12-17 13:20:30 +0100

    Adding second way of monitoring bandwidth

--- monitor_bandwidth.py
+++ monitor_bandwidth.py
@@ -2,8 +2,12 @@
 import sys
 from time import sleep
 from datetime import datetime
+import psutil
 
 def process_net_file(content, interface):
+    '''
+    Processes the /proc/net/dev file.
+    '''
     res = ""
     for line in content.split('\n'):
         if interface in line:
@@ -16,12 +20,23 @@ def process_net_file(content, interface):
     return int(incoming), int(outgoing)
 
 def get_crnt_net_data(interface):
+    '''
+    Uses /proc/net/dev to get the number of bytes sent and received.
+    '''
     content = ""
     with open("/proc/net/dev", "r") as net_file:
         content = net_file.read()
     return process_net_file(content, interface)
 
-def main(interface, outgoing_file, incoming_file):
+def get_crnt_psutil_data(interface):
+    '''
+    Uses psutil to extract the number of bytes sent and received.
+    '''
+    current_stats = psutil.net_io_counters(pernic=True)
+    interface_stats = current_stats[interface]
+    return int(interface_stats.bytes_recv), int(interface_stats.bytes_send)
+
+def main(interface, outgoing_file, incoming_file, method):
     out_file = open(outgoing_file, "w")
     in_file = open(incoming_file, "w")
     old_in, old_out = get_crnt_net_data(interface)
@@ -42,11 +57,12 @@ def main(interface, outgoing_file, incoming_file):
         in_file.flush()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("usage: monitor_bw.py INTERFACE OUTGOING_FILE INCOMING_FILE\n")
+    if len(sys.argv) != 5:
+        print("usage: monitor_bw.py INTERFACE OUTGOING_FILE INCOMING_FILE METHOD\n")
     else:
         interface = sys.argv[1]
         outgoing_file = sys.argv[2]
         incoming_file = sys.argv[3]
-        main(interface, outgoing_file, incoming_file)
+	method = sys.argv[4]
+        main(interface, outgoing_file, incoming_file, method)
 
