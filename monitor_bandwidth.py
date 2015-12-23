7e39f8db5624f4cebf4345f8c57ae655dd196713
Ove Danner <ovedanner@gmail.com>
2015-12-23 11:12:35 +0100

    Added extra argument to monitor_bandwidth to specify the index of the outgoing bytes when parsing /proc/net/dev. This is different on CentOS and Ubuntu for instance

--- monitor_bandwidth.py
+++ monitor_bandwidth.py
@@ -4,7 +4,7 @@ from time import sleep
 from datetime import datetime
 import psutil
 
-def process_net_file(content, interface):
+def process_net_file(content, interface, proc_index):
     '''
     Processes the /proc/net/dev file.
     '''
@@ -16,17 +16,17 @@ def process_net_file(content, interface):
     res = ' '.join(res.split())
     data_list = res.split(' ')
     incoming = data_list[1]
-    outgoing = data_list[9]
+    outgoing = data_list[proc_index]
     return int(incoming), int(outgoing)
 
-def get_crnt_net_data(interface):
+def get_crnt_net_data(interface, proc_index):
     '''
     Uses /proc/net/dev to get the number of bytes sent and received.
     '''
     content = ""
     with open("/proc/net/dev", "r") as net_file:
         content = net_file.read()
-    return process_net_file(content, interface)
+    return process_net_file(content, interface, proc_index)
 
 def get_crnt_psutil_data(interface):
     '''
@@ -36,17 +36,17 @@ def get_crnt_psutil_data(interface):
     interface_stats = current_stats[interface]
     return int(interface_stats.bytes_recv), int(interface_stats.bytes_sent)
 
-def main(interface, outgoing_file, incoming_file, method):
+def main(interface, outgoing_file, incoming_file, method, proc_index):
     out_file = open(outgoing_file, "w")
     in_file = open(incoming_file, "w")
     if method == 'proc':
-        old_in, old_out = get_crnt_net_data(interface)
+        old_in, old_out = get_crnt_net_data(interface, proc_index)
     else:
         old_in, old_out = get_crnt_psutil_data(interface)
     while 1:
         sleep(1)
         if method == 'proc':
-            new_in, new_out = get_crnt_net_data(interface)
+            new_in, new_out = get_crnt_net_data(interface, proc_index)
         else:
             new_in, new_out = get_crnt_psutil_data(interface)
         diff_in = new_in - old_in
@@ -63,11 +63,15 @@ def main(interface, outgoing_file, incoming_file, method):
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
-        print("usage: monitor_bw.py INTERFACE OUTGOING_FILE INCOMING_FILE METHOD\n")
+        print("usage: monitor_bw.py INTERFACE OUTGOING_FILE INCOMING_FILE METHOD [PROC_INDEX]\n")
     else:
         interface = sys.argv[1]
         outgoing_file = sys.argv[2]
         incoming_file = sys.argv[3]
 	method = sys.argv[4]
-        main(interface, outgoing_file, incoming_file, method)
+	if len(sys.argv) == 6:
+		proc_index = int(sys.argv[5])
+	else:
+		proc_index = 9	
+        main(interface, outgoing_file, incoming_file, method, proc_index)
 
